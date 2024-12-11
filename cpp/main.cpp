0f830401cef115e02b4a573335066b64597b86ff
gnuprivacyguard <179612986+gnuprivacyguard@users.noreply.github.com>
2024-12-11 14:53:58 +0100

    tld for last 4 commitsr: new csv format cause of discord requirements spaces -> tabs

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -7,43 +7,43 @@
 #include "nlohmann/json.hpp" 
 
 int main() {
-    std::unordered_map<std::string, std::vector<std::string>> channelsMessages;
-    std::string currentPath = std::filesystem::current_path().string();
-    
-    // Get all directories in the current path
-    for (const auto& entry : std::filesystem::directory_iterator(currentPath)) {
-        if (std::filesystem::is_directory(entry.status())) {
-            std::string channel = entry.path().filename().string();
-            std::string messagesFilePath = currentPath + "/" + channel + "/messages.json";
-            
-            // Open the json file
-            std::ifstream messagesFiles(messagesFilePath);
-            nlohmann::json messagesJSON;
-            messagesFiles >> messagesJSON;
-            std::vector<std::string> messages;
-                
-            // Extract message IDs as strings
-            for (const auto& message : messagesJSON) {
-                messages.push_back(std::to_string(message["ID"].get<uint64_t>()));
-            }
-                
-            // Only add channels with non-empty messages
-            if (!messages.empty()) {
-                channelsMessages[channel] = messages;
-            }
-            
-            messagesFiles.close();
-        }
-    }
+	std::unordered_map<std::string, std::vector<std::string>> channelsMessages;
+	std::string currentPath = std::filesystem::current_path().string();
+		
+	// Get all directories in the current path
+	for (const auto& entry : std::filesystem::directory_iterator(currentPath)) {
+		if (std::filesystem::is_directory(entry.status())) {
+			std::string channel = entry.path().filename().string();
+			std::string messagesFilePath = currentPath + "/" + channel + "/messages.json";
+			
+			// Open the json file
+			std::ifstream messagesFiles(messagesFilePath);
+			nlohmann::json messagesJSON;
+			messagesFiles >> messagesJSON;
+			std::vector<std::string> messages;
+				
+			// Extract message IDs as strings
+			for (const auto& message : messagesJSON) {
+				messages.push_back(std::to_string(message["ID"].get<uint64_t>()));
+			}
+				
+			// Only add channels with non-empty messages
+			if (!messages.empty()) {
+				channelsMessages[channel] = messages;
+			}
+			
+			messagesFiles.close();
+		}
+	}
 
-    // Write IDs into sensitive.txt
-    std::ofstream outputFile(currentPath + "/sensitive.csv");
-    for (const auto& [channelIDs, messages] : channelsMessages) {
+	// Write IDs into sensitive.txt
+	std::ofstream outputFile(currentPath + "/sensitive.csv");
+	for (const auto& [channelIDs, messages] : channelsMessages) {
 		for (size_t i = 0; i < messages.size(); ++i) {
-        	outputFile << channelIDs << "," << messages[i] << "\n";
-        }
-        outputFile << "\n";
-    }
-        outputFile.close();
-    return 0;
+			outputFile << channelIDs << "," << messages[i] << "\n";
+		}
+		outputFile << "\n";
+	}
+		outputFile.close();
+	return 0;
 }
--- py/main.py
+++ py/main.py
@@ -7,29 +7,29 @@ messages_dir = os.path.dirname(__file__)
 
 # Get all directories from current path
 channels = [
-    channel
-    for channel in os.listdir(messages_dir)
-    if os.path.isdir(f"{messages_dir}/{channel}")
+	channel
+	for channel in os.listdir(messages_dir)
+	if os.path.isdir(f"{messages_dir}/{channel}")
 ]
 
 # Add all messages from json to a dictionary
 for channel in channels:
-    messages_file = open(f"{messages_dir}/{channel}/messages.json", encoding="utf8")
+	messages_file = open(f"{messages_dir}/{channel}/messages.json", encoding="utf8")
 
-    messages = json.loads(messages_file.read())
-    messages = [str(message["ID"]) for message in messages]
+	messages = json.loads(messages_file.read())
+	messages = [str(message["ID"]) for message in messages]
 
-    # Add non-empty channels
-    if messages:
-        channels_messages[channel] = messages
-    
-    messages_file.close()
+	# Add non-empty channels
+	if messages:
+		channels_messages[channel] = messages
+		
+	messages_file.close()
 
 # Write each message with its channel ID and message ID in the new format
 all = open(messages_dir + "/sensitive.csv", "w", encoding="utf8")
 
 for channel_id, messages in channels_messages.items():
-    for message_id in messages:
-        all.write(f"{channel_id},{message_id}\n")  # Write channel ID and message ID on the same line, then newline
-
+	for message_id in messages:
+		all.write(f"{channel_id},{message_id}\n")  # Write channel ID and message ID on the same line, then newline
+	all.write("\n")
 all.close()
